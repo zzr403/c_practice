2fb2b5d2dd83e20aa146c8d90f78d10d9aa35a30
zzr403 <54354904+zzr403@users.noreply.github.com>
2021-04-19 07:19:26 +0800

    master

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//#include <cstdlib>
+#include <math.h>
+int main()
+{
+    int a;
+    printf("��ã�");
+    scanf("%d", &a);
+    printf("%d\n", a);
+    system("pause");
+    return 0;
+}
